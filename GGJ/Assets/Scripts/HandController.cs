4e57a4983bc7cf53f1b0026d36797f7576be8d0c
Aleksander Szamałek <aleksander.szamalek@gmail.com>
2020-01-31 19:44:35 +0100

    added IGrabbable interface

--- GGJ/Assets/Scripts/HandController.cs
+++ GGJ/Assets/Scripts/HandController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandController : MonoBehaviour
-{
+public class HandController : MonoBehaviour {
+
+    private bool isGrabbing = false;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
